ccb335e5e35180b9681f54958517089a246c88bb
PopperVids <uribealexis917@gmail.com>
2024-11-12 22:15:49 -0800

    Css and Rust are hard😭

--- Att-Mod-Panel/src-tauri/src/lib.rs
+++ Att-Mod-Panel/src-tauri/src/lib.rs
@@ -1,17 +1,18 @@
 #[tauri::command]
-fn user_name(user_name: &str) -> String {
-    format!("Logged in as {}", user_name)
+fn login_user(login_user: &str) -> String {
+    format!("Logged in as {}", login_user)
 }
+
 #[tauri::command]
-fn pass_word(pass_word: &str) -> String {
-    format!("with password {}", pass_word)
+fn login_pass(login_pass: &str) -> String {
+    format!("with password {}", login_pass)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![user_name, pass_word])
+        .invoke_handler(tauri::generate_handler![login_user, login_pass])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- Att-Mod-Panel/src/main.ts
+++ Att-Mod-Panel/src/main.ts
@@ -2,21 +2,35 @@ import { invoke } from "@tauri-apps/api/core";
 
 let userInputEl: HTMLInputElement | null;
 let userMsgEl: HTMLElement | null;
+let passInputEl: HTMLInputElement | null;
+let passMsgEl: HTMLInputElement | null;
 
 async function user() {
   if (userMsgEl && userInputEl) {
     // Ensure the key matches what the Rust command expects
-    userMsgEl.textContent = await invoke("user_name", {
-      userName: userInputEl.value, // Updated key
+    userMsgEl.textContent = await invoke("login_user", {
+      loginUser: userInputEl.value, // Updated key
+    });
+  }
+}
+
+async function pass() {
+  if (passMsgEl && passInputEl) {
+    // Ensure the key matches what the Rust command expects
+    passMsgEl.textContent = await invoke("login_pass", {
+      loginPass: passInputEl.value, // Updated key
     });
   }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
   userInputEl = document.querySelector("#greet-input");
+  passInputEl = document.querySelector("#Password-input");
   userMsgEl = document.querySelector("#greet-msg");
+  passMsgEl = document.querySelector("#pass-msg");
   document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
     e.preventDefault();
     user();
+    pass();
   });
 });
