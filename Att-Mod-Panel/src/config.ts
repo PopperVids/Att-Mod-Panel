854b3b5abd89269cac025b0ba6264aacc9cfd0eb
PopperVids <uribealexis917@gmail.com>
2024-12-04 18:56:06 -0800

    fixy

--- Att-Mod-Panel/src/config.ts
+++ Att-Mod-Panel/src/config.ts
@@ -3,7 +3,7 @@ const CommandButton = document.getElementById("ConnectButton")
 
 CommandButton?.addEventListener(`click`, async (event) => {
   event.preventDefault();
-  await message('Connecting to server', { title: 'Att mod panel', kind: 'info',  });
+  await message('Connecting to server...', { title: 'Att mod panel', kind: 'info',  });
 })
 
 export const myUserConfig = {
